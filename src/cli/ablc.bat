d80ca032f67df720db1ebf1273952441a87d5bb5
joao furegatti (ubuntu) <joao.furegatti@casp.com.br>
2025-03-18 13:10:12 -0300

    done the ablc.bat

--- src/cli/ablc.bat
+++ src/cli/ablc.bat
@@ -1,50 +1,74 @@
 @echo off
-set prowin="%DLC%\bin\prowin.exe"
+setlocal
+set prowin=%DLC%\bin\prowin.exe
 
 goto GETOPTIONS
 
 :HELP
-
 echo USAGE:
+echo.
 echo ablc [-h]
 echo 	displays this help
 echo.
-echo ablc [-c ^<compiler-options.json^>]
+echo ablc [-n ^<name-of-file.json^>]
+echo 	copy the project template to the specified path
+echo.
+echo ablc [-c ^<compiler-options.json^>] [...OPTIONS]
 echo 	compile once according to the specification in the json file
 echo.
-echo ablc [-s ^<compiler-options.json^> -t ^<seconds^>]
-echo 	start the compiation service with the periodicity of `-t`
+echo ablc [-s ^<compiler-options.json^>] [...OPTIONS]
+echo 	starts the compilation service according with the specified json file
+echo.
+echo OPTIONS
+echo 	-ininame 	= path to the .ini file
+echo 	-p		= extra procedures to run before the compilation/compilation service
 
 goto PROGRAMEND
 
 :GETOPTIONS
-
 if /I "%1" == "-h" goto HELP
 if /I "%1" == "-c" (
 	set compilerSpecs=%2
-	goto COMPILEONCE
+	set compilerScript=%~dp0^compile-once.p
+	shift
+	shift
 )
-if /I "%1" == "-s" if "%3" == "-t" (
-	set compilerSpecs=%2 
-	set interval=%4
-	goto SERVICE
+if /I "%1" == "-s" (
+	set compilerSpecs=%2
+	set compilerScript=%~dp0^compiler-service.p
+	shift
+	shift
+)
+if /I "%1" == "-n" (
+	copy %~dp0^project-template.json %2
+	goto PROGRAMEND
+)
+if /I "%1" == "-ininame" (
+	set additionalArgs=%additionalArgs% -basekey "ini" -ininame %2
+	shift
+	shift
+)
+if /I "%1" == "-p" (
+	set extraProcedures=%2
+	shift
+	shift
 )
 
-goto HELP
+shift
+if /I "%1" == "" (
+	goto RUNCOMPILATIONCMD
+)
 
-:COMPILEONCE
-echo "compile once called"
-echo %compilerSpecs%
+goto GETOPTIONS
 
-goto PROGRAMEND
-
-:SERVICE
-echo "compiler service called"
-echo %compilerSpecs%
-echo %interval%
-
+:RUNCOMPILATIONCMD
+if "%compilerScript%" == "" (
+	goto HELP
+)
+%prowin% -p %compilerScript% -param %compilerSpecs%,%extraProcedures% %additionalArgs%
 
 goto PROGRAMEND
 
 :PROGRAMEND
+endlocal
 @echo on
